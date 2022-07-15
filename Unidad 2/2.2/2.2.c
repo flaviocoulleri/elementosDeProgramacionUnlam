2c96ca96e557b9fc4df67220ca1e1fc3fdbac47f
flaviocoulleri <flavionicolascoulleri@gmail.com>
2022-07-15 01:48:24 -0300

    Unidad 2

---
+++ Unidad 2/2.2/2.2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    int nota1,nota2;
+    float promedio;
+    printf("Ingresar la nota 1: ");
+    scanf("%d",&nota1);
+    printf("Ingresar la nota 2: ");
+    scanf("%d",&nota2);
+    promedio=(nota1+nota2)/2;
+    printf("El promedio es: %.f",promedio);
+}
