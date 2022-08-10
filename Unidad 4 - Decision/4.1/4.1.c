4c5ad8f6f9b344a4f43b89f15f49f403fa1a13de
flaviocoulleri <flavionicolascoulleri@gmail.com>
2022-08-09 23:49:45 -0300

    Unidad 4

---
+++ Unidad 4 - Decision/4.1/4.1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main()
+{
+    int nro1,nro2,nro3;
+    printf("Ingresar numero 1: ");
+    scanf("%d",&nro1);
+    printf("Ingresar numero 2: ");
+    scanf("%d",&nro2);
+    printf("Ingresar numero 3: ");
+    scanf("%d",&nro3);
+    if(nro1>nro2&&nro1>nro3)
+    {
+        printf("El numero 1 es mayor");
+    }
+    else
+        if(nro2>nro1&&nro2>nro3)
+        {
+            printf("El numero 2 es mayor");
+        }
+        else
+            printf("El numero 3 es mayor");
+
+}
